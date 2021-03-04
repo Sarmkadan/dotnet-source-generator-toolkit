6c4a3cb094a4e7096d71ddd2fb7f15204f4e2ff1
Vladyslav Zaiets <rutova2@gmail.com>
2021-03-04 18:48:49 +0200

    HealthCheckEndpoint: null/range checks

--- HealthCheckEndpoint.cs
+++ HealthCheckEndpoint.cs
@@ -3,12 +3,24 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace DotNetSourceGeneratorToolkit
 {
+    /// <summary>
+    /// Represents a health check endpoint.
+    /// </summary>
     public sealed class HealthCheckEndpoint
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthCheckEndpoint"/> class.
+        /// </summary>
         public HealthCheckEndpoint()
         {
         }
 
+        /// <summary>
+        /// Checks the health of the system.
+        /// </summary>
+        /// <param name="context">The health check context.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A health check result.</returns>
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             // Implement health check logic here
