0d39d5b5d0d777fbc39ff3bcde398c6497773730
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-16 20:10:41 +0300

    json serialization for MapperGeneratorServiceJson

---
+++ Services/MapperGeneratorServiceJsonExtensions.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetSourceGeneratorToolkit.Services;
+
+public static class MapperGeneratorServiceJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="MapperGeneratorService"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The service instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the service.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this MapperGeneratorService value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="MapperGeneratorService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized service instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static MapperGeneratorService? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        return json.IsNullOrWhiteSpace()
+            ? null
+            : JsonSerializer.Deserialize<MapperGeneratorService>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="MapperGeneratorService"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized service instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out MapperGeneratorService? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (json.IsNullOrWhiteSpace())
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<MapperGeneratorService>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
+
+file static class StringExtensions
+{
+    public static bool IsNullOrWhiteSpace(this string? s) => string.IsNullOrWhiteSpace(s);
+}
