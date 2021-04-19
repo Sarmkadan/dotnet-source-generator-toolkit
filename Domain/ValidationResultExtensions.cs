66276d083751cd1d9f9d1f0eec267b86eab4c955
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-19 11:36:15 +0300

    feat: add extension helpers for ValidationResult.

---
+++ Domain/ValidationResultExtensions.cs
@@ -0,0 +1,46 @@
+namespace DotNetSourceGeneratorToolkit.Domain;
+
+/// <summary>
+/// Provides extension methods for <see cref="ValidationResult"/>.
+/// </summary>
+public static class ValidationResultExtensions
+{
+    /// <summary>
+    /// Determines whether the validation result has any errors or warnings.
+    /// </summary>
+    /// <param name="validationResult">The validation result.</param>
+    /// <returns><c>true</c> if the validation result has any errors or warnings; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="validationResult"/> is <c>null</c>.</exception>
+    public static bool HasMessages(this ValidationResult validationResult)
+    {
+        ArgumentNullException.ThrowIfNull(validationResult);
+
+        return validationResult.Errors.Count > 0 || validationResult.Warnings.Count > 0;
+    }
+
+    /// <summary>
+    /// Gets a read-only list of all messages (errors and warnings) in the validation result.
+    /// </summary>
+    /// <param name="validationResult">The validation result.</param>
+    /// <returns>A read-only list of all messages.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="validationResult"/> is <c>null</c>.</exception>
+    public static IReadOnlyList<string> GetAllMessagesList(this ValidationResult validationResult)
+    {
+        ArgumentNullException.ThrowIfNull(validationResult);
+
+        return validationResult.Errors.Concat(validationResult.Warnings).ToList().AsReadOnly();
+    }
+
+    /// <summary>
+    /// Determines whether the validation result is valid and has no warnings.
+    /// </summary>
+    /// <param name="validationResult">The validation result.</param>
+    /// <returns><c>true</c> if the validation result is valid and has no warnings; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="validationResult"/> is <c>null</c>.</exception>
+    public static bool IsValidAndClean(this ValidationResult validationResult)
+    {
+        ArgumentNullException.ThrowIfNull(validationResult);
+
+        return validationResult.IsValid && !validationResult.HasMessages();
+    }
+}
