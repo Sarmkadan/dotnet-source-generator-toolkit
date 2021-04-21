0c81565b5599797910733c5dd03c921429100494
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-21 14:41:09 +0300

    Entity extensions
    one-liner really but wanted a clean commit.

---
+++ Domain/EntityExtensions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace DotNetSourceGeneratorToolkit.Domain
+{
+    /// <summary>
+    /// Provides extension methods for the <see cref="Entity"/> type.
+    /// </summary>
+    public static class EntityExtensions
+    {
+        /// <summary>
+        /// Gets the fully qualified name of the entity (namespace + name).
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>The qualified name.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="entity"/>.<see cref="Entity.Namespace"/> or <see cref="Entity.Name"/> is <c>null</c> or empty.
+        /// </exception>
+        public static string GetQualifiedName(this Entity entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+            ArgumentException.ThrowIfNullOrEmpty(entity.Namespace);
+            ArgumentException.ThrowIfNullOrEmpty(entity.Name);
+            return $"{entity.Namespace}.{entity.Name}";
+        }
+
+        /// <summary>
+        /// Determines whether the entity has the specified attribute.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <param name="attribute">The attribute name to check.</param>
+        /// <returns><c>true</c> if the attribute is present; otherwise <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> or <paramref name="attribute"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="attribute"/> is empty.</exception>
+        public static bool HasAttribute(this Entity entity, string attribute)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+            ArgumentException.ThrowIfNullOrEmpty(attribute);
+            return entity.Attributes.Contains(attribute);
+        }
+
+        /// <summary>
+        /// Gets all validation messages for the entity as a read‑only list.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>A read‑only list of validation messages.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> is <c>null</c>.</exception>
+        public static IReadOnlyList<string> GetValidationMessages(this Entity entity) =>
+            (entity ?? throw new ArgumentNullException(nameof(entity))).Validate().ToList().AsReadOnly();
+
+        /// <summary>
+        /// Creates a concise summary string for the entity.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>A summary string.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> is <c>null</c>.</exception>
+        public static string GetSummary(this Entity entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+            var access = entity.AccessModifier.ToString().ToLowerInvariant();
+            var modifiers = entity.IsAbstract ? "abstract " : entity.IsSealed ? "sealed " : string.Empty;
+            var primaryKey = entity.GetPrimaryKeyProperty()?.ToString() ?? "none";
+            var propertyCount = entity.Properties.Count;
+            return string.Format(
+                CultureInfo.InvariantCulture,
+                "{0} {1}{2} (PK: {3}, Props: {4})",
+                access,
+                modifiers,
+                entity.Name,
+                primaryKey,
+                propertyCount);
+        }
+    }
+}
