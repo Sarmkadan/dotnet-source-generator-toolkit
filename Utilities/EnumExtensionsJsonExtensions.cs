1e546e8d0abd24d9058cfbed32b2c4a86d3aaa1c
Vladyslav Zaiets <rutova2@gmail.com>
2021-04-29 18:48:02 +0300

    feat: EnumExtensionsJson (de)serialization helpers

---
+++ Utilities/EnumExtensionsJsonExtensions.cs
@@ -0,0 +1,87 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DotNetSourceGeneratorToolkit.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for enum values.
+/// </summary>
+public static class EnumExtensionsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver(),
+        WriteIndented = false
+    };
+
+    /// <summary>
+    /// Serializes an enum value to a JSON string.
+    /// </summary>
+    /// <typeparam name="T">The enum type.</typeparam>
+    /// <param name="value">The enum value to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the enum value.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when value is null.</exception>
+    public static string ToJson<T>(this T value, bool indented = false) where T : struct, Enum
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes an enum value from a JSON string.
+    /// </summary>
+    /// <typeparam name="T">The enum type to deserialize.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized enum value, or null if the string is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static T? FromJson<T>(string json) where T : struct, Enum
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<T>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize an enum value from a JSON string.
+    /// </summary>
+    /// <typeparam name="T">The enum type to deserialize.</typeparam>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized enum value, or null if deserialization fails.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson<T>(string json, out T? value) where T : struct, Enum
+    {
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<T>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
