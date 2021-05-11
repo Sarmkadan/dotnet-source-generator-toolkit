bc47d308289b0e77b41acbfc58de07db52697113
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-11 08:19:32 +0300

    handy extensions for GenerationException

--- Exceptions/GenerationExceptionExtensions.cs
+++ Exceptions/GenerationExceptionExtensions.cs
@@ -13,23 +13,40 @@ namespace DotNetSourceGeneratorToolkit.Exceptions
         /// <param name="exception">The generation exception to check.</param>
         /// <param name="generatorType">The generator type to compare against.</param>
         /// <returns>True if the generator type matches (case-insensitive); otherwise, false.</returns>
-        public static bool IsGeneratorType(this GenerationException exception, string generatorType) 
-            => exception.GeneratorType?.Equals(generatorType, StringComparison.OrdinalIgnoreCase) ?? false;
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="generatorType"/> is <see langword="null"/>.</exception>
+        public static bool IsGeneratorType(this GenerationException exception, string generatorType)
+        {
+            ArgumentNullException.ThrowIfNull(exception);
+            ArgumentNullException.ThrowIfNull(generatorType);
+
+            return exception.GeneratorType?.Equals(generatorType, StringComparison.OrdinalIgnoreCase) ?? false;
+        }
 
         /// <summary>
         /// Gets a formatted error message that includes the GeneratorType and EntityName details.
         /// </summary>
         /// <param name="exception">The generation exception.</param>
         /// <returns>A string containing the original message combined with the GeneratorType and EntityName.</returns>
-        public static string GetErrorMessageWithDetails(this GenerationException exception) 
-            => $"{exception.Message} (GeneratorType: {exception.GeneratorType}, EntityName: {exception.EntityName})";
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
+        public static string GetErrorMessageWithDetails(this GenerationException exception)
+        {
+            ArgumentNullException.ThrowIfNull(exception);
+
+            return $"{exception.Message} (GeneratorType: {exception.GeneratorType}, EntityName: {exception.EntityName})";
+        }
 
         /// <summary>
         /// Checks if the exception has an associated entity name.
         /// </summary>
         /// <param name="exception">The generation exception to check.</param>
         /// <returns>True if the EntityName is not null, empty, or whitespace; otherwise, false.</returns>
-        public static bool HasEntityName(this GenerationException exception) 
-            => !string.IsNullOrWhiteSpace(exception.EntityName);
+        /// <exception cref="ArgumentNullException"><paramref name="exception"/> is <see langword="null"/>.</exception>
+        public static bool HasEntityName(this GenerationException exception)
+        {
+            ArgumentNullException.ThrowIfNull(exception);
+
+            return !string.IsNullOrWhiteSpace(exception.EntityName);
+        }
     }
 }
