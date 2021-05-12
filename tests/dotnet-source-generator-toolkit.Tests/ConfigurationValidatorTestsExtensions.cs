62e802abac8d3bd3493ab3e748770776ea153d32
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-12 19:50:36 +0300

    A couple of ConfigurationValidator extensions

--- tests/dotnet-source-generator-toolkit.Tests/ConfigurationValidatorTestsExtensions.cs
+++ tests/dotnet-source-generator-toolkit.Tests/ConfigurationValidatorTestsExtensions.cs
@@ -3,40 +3,53 @@ using DotNetSourceGeneratorToolkit.Configuration;
 
 public static class ConfigurationValidatorTestsExtensions
 {
+    /// <summary>
+    /// Verifies that the validator can successfully retrieve default configuration values.
+    /// </summary>
+    /// <param name="tests">The test instance.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> is null.</exception>
     public static void VerifyDefaults(this ConfigurationValidatorTests tests)
     {
-        // Arrange
-        var validator = new ConfigurationValidator();
+        ArgumentNullException.ThrowIfNull(tests);
 
-        // Act
+        var validator = new ConfigurationValidator();
         var defaults = validator.GetDefaults();
 
-        // Assert
         Assert.NotNull(defaults);
     }
 
+    /// <summary>
+    /// Verifies that validation fails for invalid options and returns appropriate error messages.
+    /// </summary>
+    /// <param name="tests">The test instance.</param>
+    /// <param name="invalidOptions">The invalid options to validate.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> or <paramref name="invalidOptions"/> is null.</exception>
     public static void VerifyValidationFailure(this ConfigurationValidatorTests tests, ToolkitOptions invalidOptions)
     {
-        // Arrange
-        var validator = new ConfigurationValidator();
+        ArgumentNullException.ThrowIfNull(tests);
+        ArgumentNullException.ThrowIfNull(invalidOptions);
 
-        // Act
+        var validator = new ConfigurationValidator();
         var result = validator.Validate(invalidOptions);
 
-        // Assert
         Assert.False(result.IsValid);
         Assert.NotEmpty(result.Errors);
     }
 
+    /// <summary>
+    /// Verifies that validation succeeds for valid options and returns no errors.
+    /// </summary>
+    /// <param name="tests">The test instance.</param>
+    /// <param name="validOptions">The valid options to validate.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="tests"/> or <paramref name="validOptions"/> is null.</exception>
     public static void VerifyValidationSuccess(this ConfigurationValidatorTests tests, ToolkitOptions validOptions)
     {
-        // Arrange
-        var validator = new ConfigurationValidator();
+        ArgumentNullException.ThrowIfNull(tests);
+        ArgumentNullException.ThrowIfNull(validOptions);
 
-        // Act
+        var validator = new ConfigurationValidator();
         var result = validator.Validate(validOptions);
 
-        // Assert
         Assert.True(result.IsValid);
         Assert.Empty(result.Errors);
     }
