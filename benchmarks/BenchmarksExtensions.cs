a263e5853e854442b974b05fa3e0909e3dcf2f55
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-12 09:37:29 +0300

    feat: handy extensions for Benchmarks

--- benchmarks/BenchmarksExtensions.cs
+++ benchmarks/BenchmarksExtensions.cs
@@ -1,5 +1,8 @@
 #nullable enable
 
+using System;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
@@ -7,20 +10,38 @@ using BenchmarkDotNet.Running;
 namespace DotNetSourceGeneratorToolkit.Benchmarks;
 
 /// <summary>
-/// Extension methods for the Benchmarks class providing additional functionality
-/// for benchmark analysis, reporting, and configuration management
+/// Extension methods for <see cref="BenchmarkDotNet.Running.BenchmarkRunner"/> providing additional functionality
+/// for benchmark analysis, reporting, and configuration management.
 /// </summary>
+/// <remarks>
+/// All public methods validate their parameters using <see cref="ArgumentNullException.ThrowIfNull"/>.
+/// </remarks>
 public static class BenchmarksExtensions
 {
     /// <summary>
-    /// Formats benchmark results as a markdown table for documentation purposes
+    /// Validates that the benchmarks parameter is not null.
     /// </summary>
-    /// <param name="benchmarks">The benchmarks instance</param>
-    /// <param name="results">Benchmark results to format</param>
-    /// <returns>Markdown formatted table with benchmark results</returns>
+    /// <param name="benchmarks">The benchmarks instance to validate.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="benchmarks"/> is <see langword="null"/></exception>
+    private static void ValidateBenchmarksParameter(Benchmarks benchmarks)
+    {
+        ArgumentNullException.ThrowIfNull(benchmarks);
+    }
+
+    /// <summary>
+    /// Formats benchmark results as a markdown table for documentation purposes.
+    /// </summary>
+    /// <param name="benchmarks">The <see cref="Benchmarks"/> instance.</param>
+    /// <param name="results">Benchmark results to format.</param>
+    /// <returns>Markdown formatted table with benchmark results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="results"/> is <see langword="null"/>.</exception>
     public static string FormatBenchmarkResultsAsMarkdown(this Benchmarks benchmarks, Summary results)
     {
-        if (results == null || results.Benchmarks == null || !results.Benchmarks.Any())
+        ValidateBenchmarksParameter(benchmarks);
+        ArgumentNullException.ThrowIfNull(results);
+        ArgumentNullException.ThrowIfNull(results.Benchmarks);
+
+        if (!results.Benchmarks.Any())
         {
             return "No benchmark results available.";
         }
@@ -35,12 +56,12 @@ public static class BenchmarksExtensions
             var statistics = benchmark.ResultStatistics;
             var memory = benchmark.Memory;
 
-            var mean = statistics?.Mean.ToString("N3") ?? "N/A";
-            var error = statistics?.StandardError.ToString("N3") ?? "N/A";
-            var stdDev = statistics?.StandardDeviation.ToString("N3") ?? "N/A";
-            var median = statistics?.Median.ToString("N3") ?? "N/A";
+            var mean = statistics?.Mean.ToString("N3", CultureInfo.InvariantCulture) ?? "N/A";
+            var error = statistics?.StandardError.ToString("N3", CultureInfo.InvariantCulture) ?? "N/A";
+            var stdDev = statistics?.StandardDeviation.ToString("N3", CultureInfo.InvariantCulture) ?? "N/A";
+            var median = statistics?.Median.ToString("N3", CultureInfo.InvariantCulture) ?? "N/A";
             var gen0 = memory?.GetValue(GcStats.Gen0Collections)?.ToString() ?? "N/A";
-            var allocated = memory?.GetValue(GcStats.BytesAllocatedPerOperation)?.ToString("N0") ?? "N/A";
+            var allocated = memory?.GetValue(GcStats.BytesAllocatedPerOperation)?.ToString("N0", CultureInfo.InvariantCulture) ?? "N/A";
 
             sb.AppendLine($"| {method} | {mean} | {error} | {stdDev} | {median} | {gen0} | {allocated} |");
         }
@@ -49,50 +70,65 @@ public static class BenchmarksExtensions
     }
 
     /// <summary>
-    /// Gets the slowest benchmark from the results
+    /// Gets the slowest benchmark from the results.
     /// </summary>
-    /// <param name="benchmarks">The benchmarks instance</param>
-    /// <param name="results">Benchmark results to analyze</param>
-    /// <returns>Benchmark with the highest mean execution time, or null if no results</returns>
+    /// <param name="benchmarks">The <see cref="Benchmarks"/> instance.</param>
+    /// <param name="results">Benchmark results to analyze.</param>
+    /// <returns>Benchmark with the highest mean execution time, or <see langword="null"/> if no results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="results"/> is <see langword="null"/>.</exception>
     public static BenchmarkReport? GetSlowestBenchmark(this Benchmarks benchmarks, Summary results)
     {
-        if (results?.Benchmarks == null || !results.Benchmarks.Any())
+        ValidateBenchmarksParameter(benchmarks);
+        ArgumentNullException.ThrowIfNull(results);
+        ArgumentNullException.ThrowIfNull(results.Benchmarks);
+
+        if (!results.Benchmarks.Any())
         {
             return null;
         }
 
         return results.Benchmarks
-            .OrderByDescending(b => b.ResultStatistics?.Mean)
+            .OrderByDescending(b => b.ResultStatistics?.Mean ?? double.MinValue)
             .First();
     }
 
     /// <summary>
-    /// Gets the fastest benchmark from the results
+    /// Gets the fastest benchmark from the results.
     /// </summary>
-    /// <param name="benchmarks">The benchmarks instance</param>
-    /// <param name="results">Benchmark results to analyze</param>
-    /// <returns>Benchmark with the lowest mean execution time, or null if no results</returns>
+    /// <param name="benchmarks">The <see cref="Benchmarks"/> instance.</param>
+    /// <param name="results">Benchmark results to analyze.</param>
+    /// <returns>Benchmark with the lowest mean execution time, or <see langword="null"/> if no results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="results"/> is <see langword="null"/>.</exception>
     public static BenchmarkReport? GetFastestBenchmark(this Benchmarks benchmarks, Summary results)
     {
-        if (results?.Benchmarks == null || !results.Benchmarks.Any())
+        ValidateBenchmarksParameter(benchmarks);
+        ArgumentNullException.ThrowIfNull(results);
+        ArgumentNullException.ThrowIfNull(results.Benchmarks);
+
+        if (!results.Benchmarks.Any())
         {
             return null;
         }
 
         return results.Benchmarks
-            .OrderBy(b => b.ResultStatistics?.Mean)
+            .OrderBy(b => b.ResultStatistics?.Mean ?? double.MaxValue)
             .First();
     }
 
     /// <summary>
-    /// Calculates the total execution time for all benchmarks in the results
+    /// Calculates the total execution time for all benchmarks in the results.
     /// </summary>
-    /// <param name="benchmarks">The benchmarks instance</param>
-    /// <param name="results">Benchmark results to analyze</param>
-    /// <returns>Total execution time in milliseconds, or 0 if no results</returns>
+    /// <param name="benchmarks">The <see cref="Benchmarks"/> instance.</param>
+    /// <param name="results">Benchmark results to analyze.</param>
+    /// <returns>Total execution time in milliseconds, or 0 if no results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="results"/> is <see langword="null"/>.</exception>
     public static double CalculateTotalExecutionTime(this Benchmarks benchmarks, Summary results)
     {
-        if (results?.Benchmarks == null || !results.Benchmarks.Any())
+        ValidateBenchmarksParameter(benchmarks);
+        ArgumentNullException.ThrowIfNull(results);
+        ArgumentNullException.ThrowIfNull(results.Benchmarks);
+
+        if (!results.Benchmarks.Any())
         {
             return 0;
         }
