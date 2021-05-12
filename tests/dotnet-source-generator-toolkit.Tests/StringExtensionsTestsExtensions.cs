adf9b57cbb2420d7fc6de5a9067cc4d5fab868ce
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-12 15:15:01 +0300

    a couple of StringEtxensions extensions

--- tests/dotnet-source-generator-toolkit.Tests/StringExtensionsTestsExtensions.cs
+++ tests/dotnet-source-generator-toolkit.Tests/StringExtensionsTestsExtensions.cs
@@ -11,37 +11,39 @@ namespace DotNetSourceGeneratorToolkit.Tests
     public static class StringExtensionsTestsExtensions
     {
         /// <summary>
-        /// Returns the names of all public, parameter‑less test methods declared on <see cref="StringExtensionsTests"/>.
+        /// Returns the names of all public, parameterless test methods declared on <see cref="StringExtensionsTests"/>.
         /// </summary>
         /// <param name="test">The test instance.</param>
         /// <returns>An enumerable of method names.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is null.</exception>
         public static IEnumerable<string> GetTestMethodNames(this StringExtensionsTests test)
         {
-            if (test == null) throw new ArgumentNullException(nameof(test));
+            ArgumentNullException.ThrowIfNull(test);
 
             return test.GetType()
                 .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
-                .Where(m => m.GetParameters().Length == 0 && m.ReturnType == typeof(void))
-                .Select(m => m.Name);
+                .Where(static m => m.GetParameters().Length == 0 && m.ReturnType == typeof(void))
+                .Select(static m => m.Name);
         }
 
         /// <summary>
-        /// Executes every public, parameter‑less test method on the supplied <see cref="StringExtensionsTests"/> instance.
+        /// Executes every public, parameterless test method on the supplied <see cref="StringExtensionsTests"/> instance.
         /// </summary>
         /// <param name="test">The test instance.</param>
         /// <returns>
         /// A dictionary where the key is the test method name and the value is <c>null</c> if the test succeeded,
         /// or the exception that caused the failure.
         /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="test"/> is null.</exception>
         public static Dictionary<string, Exception?> RunAllTests(this StringExtensionsTests test)
         {
-            if (test == null) throw new ArgumentNullException(nameof(test));
+            ArgumentNullException.ThrowIfNull(test);
 
             var results = new Dictionary<string, Exception?>();
 
             var methods = test.GetType()
                 .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
-                .Where(m => m.GetParameters().Length == 0 && m.ReturnType == typeof(void));
+                .Where(static m => m.GetParameters().Length == 0 && m.ReturnType == typeof(void));
 
             foreach (var method in methods)
             {
@@ -50,13 +52,13 @@ namespace DotNetSourceGeneratorToolkit.Tests
                     method.Invoke(test, null);
                     results[method.Name] = null; // success
                 }
-                catch (TargetInvocationException tie) when (tie.InnerException != null)
+                catch (TargetInvocationException tie) when (tie.InnerException is not null)
                 {
                     results[method.Name] = tie.InnerException; // unwrap the actual test failure
                 }
-                catch (Exception ex)
+                catch (Exception ex) when (ex is not TargetInvocationException)
                 {
-                    results[method.Name] = ex; // any other reflection‑related error
+                    results[method.Name] = ex; // any other reflection-related error
                 }
             }
 
