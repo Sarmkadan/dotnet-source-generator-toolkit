a51a8b268ef3c1817af6ea912dc89195ad87e9f9
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-17 12:09:27 +0300

    Oops

--- Metrics/MetricsCollectorExtensions.cs
+++ Metrics/MetricsCollectorExtensions.cs
@@ -1,5 +1,9 @@
 #nullable enable
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DotNetSourceGeneratorToolkit.Metrics;
 
 /// <summary>
@@ -14,9 +18,13 @@ public static class MetricsCollectorExtensions
     /// <param name="metricName">Name of the counter metric.</param>
     /// <param name="amount">Amount to increment by (defaults to 1).</param>
     /// <param name="tags">Optional tags to categorize the metric.</param>
-    public static void IncrementCounter(this MetricsCollector collector, string metricName, long amount, Dictionary<string, string>? tags = null)
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/> or <paramref name="metricName"/> is <see langword="null"/>.</exception>
+    public static void IncrementCounter(this MetricsCollector collector, string metricName, long amount = 1, Dictionary<string, string>? tags = null)
     {
-        if (tags != null && tags.Count > 0)
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(metricName);
+
+        if (tags is { Count: > 0 })
         {
             // Create a tagged version of the metric name
             var taggedName = $"{metricName}[{string.Join(",", tags.Select(t => $"{t.Key}={t.Value}"))}]";
@@ -35,9 +43,13 @@ public static class MetricsCollectorExtensions
     /// <param name="metricName">Name of the gauge metric.</param>
     /// <param name="value">The gauge value to record.</param>
     /// <param name="tags">Optional tags to categorize the metric.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/> or <paramref name="metricName"/> is <see langword="null"/>.</exception>
     public static void RecordGauge(this MetricsCollector collector, string metricName, long value, Dictionary<string, string>? tags = null)
     {
-        if (tags != null && tags.Count > 0)
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(metricName);
+
+        if (tags is { Count: > 0 })
         {
             // Create a tagged version of the metric name
             var taggedName = $"{metricName}[{string.Join(",", tags.Select(t => $"{t.Key}={t.Value}"))}]";
@@ -56,8 +68,14 @@ public static class MetricsCollectorExtensions
     /// <param name="operationName">Name of the operation being measured.</param>
     /// <param name="action">The action to measure.</param>
     /// <returns>The elapsed time as a TimeSpan.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/> or <paramref name="operationName"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="operationName"/> is empty or whitespace.</exception>
     public static TimeSpan MeasureTime(this MetricsCollector collector, string operationName, Action action)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(operationName);
+        ArgumentNullException.ThrowIfNull(action);
+
         using var timer = collector.StartTimer(operationName);
         action();
         return timer.Elapsed;
@@ -71,8 +89,14 @@ public static class MetricsCollectorExtensions
     /// <param name="operationName">Name of the operation being measured.</param>
     /// <param name="func">The function to measure.</param>
     /// <returns>The result of the function and the elapsed time.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/>, <paramref name="operationName"/>, or <paramref name="func"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="operationName"/> is empty or whitespace.</exception>
     public static (T Result, TimeSpan Elapsed) MeasureTime<T>(this MetricsCollector collector, string operationName, Func<T> func)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(operationName);
+        ArgumentNullException.ThrowIfNull(func);
+
         using var timer = collector.StartTimer(operationName);
         var result = func();
         return (result, timer.Elapsed);
@@ -85,8 +109,14 @@ public static class MetricsCollectorExtensions
     /// <param name="operationName">Name of the operation being measured.</param>
     /// <param name="action">The async action to measure.</param>
     /// <returns>A task representing the completion.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/>, <paramref name="operationName"/>, or <paramref name="action"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="operationName"/> is empty or whitespace.</exception>
     public static async Task MeasureTimeAsync(this MetricsCollector collector, string operationName, Func<Task> action)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(operationName);
+        ArgumentNullException.ThrowIfNull(action);
+
         using var timer = collector.StartTimer(operationName);
         await action();
     }
@@ -99,8 +129,14 @@ public static class MetricsCollectorExtensions
     /// <param name="operationName">Name of the operation being measured.</param>
     /// <param name="func">The async function to measure.</param>
     /// <returns>The result of the function and the elapsed time.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/>, <paramref name="operationName"/>, or <paramref name="func"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="operationName"/> is empty or whitespace.</exception>
     public static async Task<(T Result, TimeSpan Elapsed)> MeasureTimeAsync<T>(this MetricsCollector collector, string operationName, Func<Task<T>> func)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(operationName);
+        ArgumentNullException.ThrowIfNull(func);
+
         using var timer = collector.StartTimer(operationName);
         var result = await func();
         return (result, timer.Elapsed);
@@ -113,15 +149,22 @@ public static class MetricsCollectorExtensions
     /// <param name="metricName">Name of the histogram metric.</param>
     /// <param name="value">The value to record in the histogram.</param>
     /// <param name="context">Optional context information for the measurement.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/> or <paramref name="metricName"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="metricName"/> is empty or whitespace.</exception>
     public static void RecordHistogram(this MetricsCollector collector, string metricName, long value, string? context = null)
     {
-        if (context != null)
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(metricName);
+
+        if (context is not null)
         {
-            var contextMetricName = $"{metricName}_with_context";
+            var contextMetricName = $"{metricName}_with_context_{context.Replace(" ", "_")}";
             collector.RecordHistogram(contextMetricName, value);
         }
-
-        collector.RecordHistogram(metricName, value);
+        else
+        {
+            collector.RecordHistogram(metricName, value);
+        }
     }
 
     /// <summary>
@@ -129,8 +172,11 @@ public static class MetricsCollectorExtensions
     /// </summary>
     /// <param name="collector">The metrics collector instance.</param>
     /// <returns>A metrics snapshot.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/> is <see langword="null"/>.</exception>
     public static MetricsSnapshot GetSnapshotAndReset(this MetricsCollector collector)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+
         var snapshot = collector.GetSnapshot();
         collector.Reset();
         return snapshot;
@@ -145,8 +191,14 @@ public static class MetricsCollectorExtensions
     /// <param name="histogramName">Name of the histogram metric.</param>
     /// <param name="amount">Amount to increment the counter by.</param>
     /// <param name="durationMs">Duration value to record in the histogram.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="collector"/>, <paramref name="counterName"/>, or <paramref name="histogramName"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="counterName"/> or <paramref name="histogramName"/> is empty or whitespace.</exception>
     public static void RecordOperation(this MetricsCollector collector, string counterName, string histogramName, long amount, long durationMs)
     {
+        ArgumentNullException.ThrowIfNull(collector);
+        ArgumentException.ThrowIfNullOrEmpty(counterName);
+        ArgumentException.ThrowIfNullOrEmpty(histogramName);
+
         collector.IncrementCounter(counterName, amount);
         collector.RecordHistogram(histogramName, durationMs);
     }
