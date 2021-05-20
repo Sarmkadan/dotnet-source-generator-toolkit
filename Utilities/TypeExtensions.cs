dc04d787178c904e5690909a8740054bb8402fa2
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-20 14:12:45 +0300

    a couple of Type extensions

--- Utilities/TypeExtensions.cs
+++ Utilities/TypeExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 namespace DotNetSourceGeneratorToolkit.Utilities;
 
@@ -14,32 +14,44 @@ namespace DotNetSourceGeneratorToolkit.Utilities;
 public static class TypeExtensions
 {
     /// <summary>
-    /// Check if a type is a nullable reference type.
+    /// Check if a type is a nullable reference type or nullable value type.
     /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <returns>True if the type is nullable; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static bool IsNullable(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
         return !type.IsValueType || (Nullable.GetUnderlyingType(type) is not null);
     }
 
     /// <summary>
     /// Check if a type is a numeric type.
     /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <returns>True if the type is a numeric type; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static bool IsNumeric(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
         return type.IsValueType &&
-               (type == typeof(byte) || type == typeof(sbyte) ||
-                type == typeof(short) || type == typeof(ushort) ||
-                type == typeof(int) || type == typeof(uint) ||
-                type == typeof(long) || type == typeof(ulong) ||
-                type == typeof(float) || type == typeof(double) ||
-                type == typeof(decimal));
+            (type == typeof(byte) || type == typeof(sbyte) ||
+             type == typeof(short) || type == typeof(ushort) ||
+             type == typeof(int) || type == typeof(uint) ||
+             type == typeof(long) || type == typeof(ulong) ||
+             type == typeof(float) || type == typeof(double) ||
+             type == typeof(decimal));
     }
 
     /// <summary>
     /// Check if a type is a simple type (primitive or common value type).
     /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <returns>True if the type is a simple type; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static bool IsSimpleType(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
         var underlyingType = Nullable.GetUnderlyingType(type) ?? type;
 
         return underlyingType.IsValueType ||
@@ -51,8 +63,13 @@ public static class TypeExtensions
     /// <summary>
     /// Get a friendly name for a type.
     /// </summary>
+    /// <param name="type">The type to get the friendly name for.</param>
+    /// <returns>A friendly name representation of the type.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static string GetFriendlyName(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
+
         if (type.IsGenericType)
         {
             var genericArgs = type.GetGenericArguments()
@@ -69,13 +86,17 @@ public static class TypeExtensions
     /// <summary>
     /// Get the default value for a type.
     /// </summary>
+    /// <param name="type">The type to get the default value for.</param>
+    /// <returns>The default value for the type, or null for reference types.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static object? GetDefaultValue(this Type type)
     {
-        if (type is null)
-            return null;
+        ArgumentNullException.ThrowIfNull(type);
 
         if (type.IsValueType)
+        {
             return Activator.CreateInstance(type);
+        }
 
         return null;
     }
@@ -83,16 +104,28 @@ public static class TypeExtensions
     /// <summary>
     /// Check if a type can be assigned to another type.
     /// </summary>
+    /// <param name="type">The source type.</param>
+    /// <param name="targetType">The target type to check assignability to.</param>
+    /// <returns>True if the type can be assigned to the target type; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> or <paramref name="targetType"/> is null.</exception>
     public static bool IsAssignableTo(this Type type, Type targetType)
     {
+        ArgumentNullException.ThrowIfNull(type);
+        ArgumentNullException.ThrowIfNull(targetType);
+
         return targetType.IsAssignableFrom(type);
     }
 
     /// <summary>
     /// Get all base types in the inheritance hierarchy.
     /// </summary>
+    /// <param name="type">The type to get base types for.</param>
+    /// <returns>An enumerable of base types in the inheritance hierarchy.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static IEnumerable<Type> GetBaseTypes(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
+
         var baseType = type.BaseType;
         while (baseType is not null && baseType != typeof(object))
         {
@@ -104,10 +137,17 @@ public static class TypeExtensions
     /// <summary>
     /// Check if type is a collection type (but not string).
     /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <returns>True if the type is a collection type; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="type"/> is null.</exception>
     public static bool IsCollection(this Type type)
     {
+        ArgumentNullException.ThrowIfNull(type);
+
         if (type == typeof(string))
+        {
             return false;
+        }
 
         return typeof(System.Collections.IEnumerable).IsAssignableFrom(type);
     }
