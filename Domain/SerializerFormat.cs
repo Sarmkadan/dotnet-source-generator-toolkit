cfaeee0e78baec86f9a4082192cbaba574c1005a
Vladyslav Zaiets <rutova2@gmail.com>
2026-05-23 05:24:02 +0000

    refactor: move SerializerFormat enum to its own file

---
+++ Domain/SerializerFormat.cs
@@ -0,0 +1,15 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace DotNetSourceGeneratorToolkit.Domain;
+
+public enum SerializerFormat
+{
+    Json,
+    Xml,
+    Binary,
+}
