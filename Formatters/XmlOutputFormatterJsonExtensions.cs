e3e4c368e53212635d896b67089b0a5973deb76b
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Dedupe some logic in XmlOutputFormatterJsonExtensions

--- Formatters/XmlOutputFormatterJsonExtensions.cs
+++ Formatters/XmlOutputFormatterJsonExtensions.cs
@@ -25,10 +25,11 @@ public static class XmlOutputFormatterJsonExtensions
     /// <summary>
     /// Serializes the <see cref="XmlOutputFormatter"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The <see cref="XmlOutputFormatter"/> instance to serialize.</param>
+    /// <param name="value">The <see cref="XmlOutputFormatter"/> instance to serialize. Must not be <see langword="null"/>.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the <see cref="XmlOutputFormatter"/> instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <returns>A JSON string representation of the <see cref="XmlOutputFormatter"/> instance.
+    /// The serialized JSON includes the <see cref="XmlOutputFormatter.FileExtension"/> and <see cref="XmlOutputFormatter.FormatName"/> properties.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this XmlOutputFormatter value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -46,10 +47,10 @@ public static class XmlOutputFormatterJsonExtensions
     /// <summary>
     /// Deserializes a JSON string to an <see cref="XmlOutputFormatter"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>An <see cref="XmlOutputFormatter"/> instance deserialized from JSON.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    /// <param name="json">The JSON string to deserialize. Must not be <see langword="null"/> or empty.</param>
+    /// <returns>An <see cref="XmlOutputFormatter"/> instance deserialized from JSON, or <see langword="null"/> if the JSON represents a null value.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
+    /// <exception cref="JsonException">The JSON is invalid or cannot be deserialized to an <see cref="XmlOutputFormatter"/> instance.</exception>
     public static XmlOutputFormatter? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -60,10 +61,10 @@ public static class XmlOutputFormatterJsonExtensions
     /// <summary>
     /// Attempts to deserialize a JSON string to an <see cref="XmlOutputFormatter"/> instance.
     /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized <see cref="XmlOutputFormatter"/> instance if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <param name="json">The JSON string to deserialize. Must not be <see langword="null"/> or empty.</param>
+    /// <param name="value">Receives the deserialized <see cref="XmlOutputFormatter"/> instance if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static bool TryFromJson(string json, out XmlOutputFormatter? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
